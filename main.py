644ea3e3a022b598de244484a6143f2c21eb9e5d
Alan McGraw <alanmcgraw@proton.me>
2024-04-04 09:49:35 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get
+def async ():
+    pass
+
+@app.put ():
+def async
+    pass
+
+@app.post ():
+def async
+    pass
+
+@app.delete ():
+def async
+    pass
