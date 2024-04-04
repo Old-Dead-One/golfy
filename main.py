23ffb6ec617ccb1eb0a64f8edcb4eddf15f20fed
Alan McGraw <alanmcgraw@proton.me>
2024-04-04 10:39:50 -0600

    Added endpoints

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
+import uuid
+
 from fastapi import FastAPI
+
+from models.player import Player
+from models.course import Course
 
 
 app = FastAPI()
 
+players: dict[uuid.UUID, Player] = {}
+courses: dict[uuid.UUID, Course] = {}
 
-@app.get
-def async ():
-    pass
 
-@app.put ():
-def async
-    pass
-
-@app.post ():
-def async
-    pass
-
-@app.delete ():
-def async
-    pass
+# Players
+@app.get("/players")
+async def player_list():
+#    pass
+#
+#@app.post ("/players")
+#async def add_player ():
+#    pass
+#
+#@app.put ("/players/{player_id}")
+#async def update_player():
+#    pass
+#
+#@app.delete ("/players/{player_id}")
+#async def remove_player ():
+#    pass
+#
+## Courses
+#@app.get("/courses")
+#async def course_list():
+#    pass
+#
+#@app.post ("/courses")
+#async def add_course():
+#    pass
+#
+#@app.put ("/courses/{course_id}")
+#async def update_course():
+#    pass
+#
+#@app.delete ("/courses/{course_id}")
+#async def remove_course():
+#    pass
