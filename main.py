aa51cebee236ffd14151a9394dac4c04cf6367a4
Alan McGraw <alanmcgraw@proton.me>
2024-04-05 08:08:34 -0600

    Create additional endpoints for players

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uuid
 
 from fastapi import FastAPI
 
-from models.player import Player
+from models.player import Player, CreatePlayerRequest
 from models.course import Course
 
 
@@ -14,12 +14,16 @@ courses: dict[uuid.UUID, Course] = {}
 
 # Players
 @app.get("/players")
-async def player_list():
-#    pass
-#
-#@app.post ("/players")
-#async def add_player ():
-#    pass
+async def list_players() -> list[Player]:
+    return players.values()
+
+@app.post ("/players")
+async def create_player(player_detail: CreatePlayerRequest) -> uuid.UUID:
+    player_id = uuid.uuid4()
+    player = Player(id=player_id, name=player_detail.name, handicap=player_detail.handicap)
+    players[player_id] = player
+    return player.id
+
 #
 #@app.put ("/players/{player_id}")
 #async def update_player():
@@ -31,12 +35,12 @@ async def player_list():
 #
 ## Courses
 #@app.get("/courses")
-#async def course_list():
-#    pass
+#async def course_list() -> list[Course]:
+#    return players.values
 #
 #@app.post ("/courses")
-#async def add_course():
-#    pass
+#async def add_course(courses: Course) -> uuid.UUID:
+#    courses[]
 #
 #@app.put ("/courses/{course_id}")
 #async def update_course():
--- models/player.py
+++ models/player.py
@@ -7,3 +7,7 @@ class Player(BaseModel):
     id: UUID
     name: str
     handicap: float
+
+class CreatePlayerRequest(BaseModel):
+    name: str
+    handicap: float
