c0ded5615a080a98357cbc30718c37319db89dc7
Alan McGraw <alanmcgraw@proton.me>
2024-04-10 08:17:25 -0600

    final commit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import uuid
 
 from fastapi import FastAPI
 
+from models.course import Course, CourseRequests, CourseResponse
 from models.player import Player, PlayerRequests, PlayerResponse
-from models.course import Course
 
 
 app = FastAPI()
@@ -11,55 +11,52 @@ app = FastAPI()
 players: dict[uuid.UUID, Player] = {}
 courses: dict[uuid.UUID, Course] = {}
 
-
-# Players
+## Players
 @app.get("/players")
-async def list_players() -> list[Player]:
+async def get_players() -> list[Player]:
     return players.values()
 
-
 @app.post("/players")
-async def create_player(player: list_players) -> uuid.UUID:
+async def create_player(player: PlayerRequests) -> PlayerResponse:
     player_id = uuid.uuid4()
-    players[player_id] = player
+    players[player_id] = Player(id=player_id, name=player.name, handicap=player.handicap)
     return PlayerResponse(id=player_id)
 
+@app.put("/players/{player_id}")
+async def update_player(player_id: uuid.UUID, updated_player: PlayerRequests) -> PlayerResponse:
+    players[player_id] = Player(id=player_id, name=updated_player.name, handicap=updated_player.handicap)
+    return PlayerResponse(id=player_id)
 
-@app.put("/players/{player_name}")
-async def update_player(player: Player) -> Player:
-    player = Player(name=player.name, handicap=player.handicap)
-    for player in players():
-        if player.name == player.name:
-            player.handicap = player.handicap
-            return player
-    return "Sorry, {player} not found."
-
-
-@app.delete ("/players/{player_name}")
-async def remove_player(player: Player):
-    player = Player(name=player.name)    
-    for player in players.items():
-        if player.name == player.name:
-            players.pop(player)
-            return "{player} has been removed."
-        return "Sorry, {player} not found."
+@app.delete("/players/{player_id}")
+async def delete_player(player_id: uuid.UUID) -> None:
+    players.pop(player_id)
 
 ## Courses
-#@app.get("/courses")
-#async def course_list() -> list[Course]:
-#    return courses.values
-#
-#@app.post ("/players")
-#async def create_player(player_detail: CreatePlayerRequest) -> uuid.UUID:
-#    player_id = uuid.uuid4()
-#    player = Player(id=player_id, name=player_detail.name, handicap=player_detail.handicap)
-#    players[player_id] = player
-#    return player.id
-#
-#@app.put ("/courses/{course_id}")
-#async def update_course():
-#    pass
-#
-#@app.delete ("/courses/{course_id}")
-#async def remove_course():
-#    pass
+@app.get("/courses")
+async def courses_list() -> list[Course]:
+    return courses.values()
+
+@app.post("/courses")
+async def create_course(new_course_request: CourseRequests ) -> CourseResponse:
+    course_id = uuid.uuid4()
+    courses[course_id] = Course(
+        id=course_id, 
+        name=new_course_request.name, 
+        location=new_course_request.location, 
+        holes=new_course_request.holes
+        )
+    return CourseResponse(id=course_id)
+
+@app.put("/courses/{course_id}")
+async def update_course(course_id: uuid.UUID, updated_course: CourseRequests) -> CourseResponse:
+    courses[course_id] = Course(
+        id=course_id, 
+        name=updated_course.name, 
+        location=updated_course.location, 
+        holes=updated_course.holes
+        )
+    return CourseResponse(id=course_id)
+
+@app.delete("/courses/{course_id}")
+async def delete_course(course_id: uuid.UUID) -> None:
+    courses.pop(course_id)
--- models/course.py
+++ models/course.py
@@ -8,3 +8,17 @@ class Course(BaseModel):
     name: str
     location: str
     holes: int
+
+
+class Course(BaseModel):
+    id: UUID
+    name: str
+    location: str
+
+class CourseRequests(BaseModel):
+    name: str
+    location: float
+    holes: int
+
+class CourseResponse(BaseModel):
+    id: UUID
